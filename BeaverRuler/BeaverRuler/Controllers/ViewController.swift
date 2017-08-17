a912010b2b433a3c4560239eac251d572a8ecb7e
darkwind <darkwinddev@gmail.com>
2017-08-17 16:38:45 +0300

    Ruler screen template

---
+++ BeaverRuler/BeaverRuler/Controllers/ViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ViewController.swift
+//  BeaverRuler
+//
+//  Created by Sasha on 8/16/17.
+//  Copyright © 2017 Sasha. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var targetImageView: UIImageView!
+    @IBOutlet weak var loadingView: UIActivityIndicatorView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var meterImageView: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+
+    fileprivate lazy var session = ARSession()
+    fileprivate lazy var sessionConfiguration = ARWorldTrackingConfiguration()
+    fileprivate lazy var isMeasuring = false;
+    fileprivate lazy var vectorZero = SCNVector3()
+    fileprivate lazy var startValue = SCNVector3()
+    fileprivate lazy var endValue = SCNVector3()
+    fileprivate lazy var lines: [RulerLine] = []
+    fileprivate var currentLine: RulerLine?
+    fileprivate lazy var unit: DistanceUnit = .centimeter
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScene()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        resetValues()
+        isMeasuring = true
+        targetImageView.image = UIImage(named: "targetGreen")
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isMeasuring = false
+        targetImageView.image = UIImage(named: "targetWhite")
+        if let line = currentLine {
+            lines.append(line)
+            currentLine = nil
+            resetButton.isHidden = false
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    // MARK: - Users Interactions
+
+    @IBAction func meterButtonTapped(_ sender: Any) {
+        let alertVC = UIAlertController(title: "Settings", message: "Please select distance unit options", preferredStyle: .actionSheet)
+        alertVC.addAction(UIAlertAction(title: DistanceUnit.centimeter.title, style: .default) { [weak self] _ in
+            self?.unit = .centimeter
+        })
+        alertVC.addAction(UIAlertAction(title: DistanceUnit.inch.title, style: .default) { [weak self] _ in
+            self?.unit = .inch
+        })
+        alertVC.addAction(UIAlertAction(title: DistanceUnit.meter.title, style: .default) { [weak self] _ in
+            self?.unit = .meter
+        })
+        alertVC.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
+
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        resetButton.isHidden = true
+        for line in lines {
+            line.removeFromParentNode()
+        }
+        lines.removeAll()
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        DispatchQueue.main.async { [weak self] in
+            self?.detectObjects()
+        }
+    }
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        messageLabel.text = "Error occurred"
+    }
+
+    func sessionWasInterrupted(_ session: ARSession) {
+        messageLabel.text = "Interrupted"
+    }
+
+    func sessionInterruptionEnded(_ session: ARSession) {
+        messageLabel.text = "Interruption ended"
+    }
+}
+
+// MARK: - Privates
+
+extension ViewController {
+    fileprivate func setupScene() {
+        targetImageView.isHidden = true
+        sceneView.delegate = self
+        sceneView.session = session
+        loadingView.startAnimating()
+        meterImageView.isHidden = true
+        messageLabel.text = "Detecting the world…"
+        resetButton.isHidden = true
+        session.run(sessionConfiguration, options: [.resetTracking, .removeExistingAnchors])
+        resetValues()
+    }
+
+    fileprivate func resetValues() {
+        isMeasuring = false
+        startValue = SCNVector3()
+        endValue =  SCNVector3()
+    }
+
+    fileprivate func detectObjects() {
+        guard let worldPosition = sceneView.realWorldVector(screenPosition: view.center) else { return }
+        targetImageView.isHidden = false
+        meterImageView.isHidden = false
+        if lines.isEmpty {
+            messageLabel.text = "Hold screen & move your phone…"
+        }
+        loadingView.stopAnimating()
+        if isMeasuring {
+            if startValue == vectorZero {
+                startValue = worldPosition
+                currentLine = RulerLine(sceneView: sceneView, startVector: startValue, unit: unit)
+            }
+            endValue = worldPosition
+            currentLine?.update(to: endValue)
+            messageLabel.text = currentLine?.distance(to: endValue) ?? "Calculating…"
+        }
+    }
+}
