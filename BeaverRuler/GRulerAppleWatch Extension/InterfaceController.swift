ba282d77edca05cacba2da5ba85d2b18a979843f
darkwind666 <darkwinddev@gmail.com>
2017-10-30 01:29:34 +0300

    make base watch ui

--- BeaverRuler/GRulerAppleWatch Extension/InterfaceController.swift
+++ BeaverRuler/GRulerAppleWatch Extension/InterfaceController.swift
@@ -8,24 +8,78 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet var measurePointsLabel: WKInterfaceLabel!
+    
+    var measure: String? {
+        didSet {
+            if let measure = measure {
+                measurePointsLabel.setText(measure)
+            }
+        }
+    }
+    
+    var session: WCSession? {
+        didSet {
+            if let session = session {
+                session.delegate = self
+                session.activate()
+            }
+        }
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    override func didAppear() {
+        super.didAppear()
+        
+        session = WCSession.default
+//        session!.sendMessage(["reference": flight.reference], replyHandler: { (response) -> Void in
+//            if let boardingPassData = response["boardingPassData"] as? NSData, boardingPass = UIImage(data: boardingPassData) {
+//                flight.boardingPass = boardingPass
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                    self.showBoardingPass()
+//                })
+//            }
+//        }, errorHandler: { (error) -> Void in
+//            print(error)
+//        })
+    }
+    
+    @IBAction func makeScreenshot() {
+        
+    }
+    
+    @IBAction func makePoint() {
+        
+    }
+    
+    @IBAction func undoPressed() {
+        
+    }
+}
 
+extension InterfaceController: WCSessionDelegate {
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: @escaping ([String : AnyObject]) -> Void) {
+
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+        
+        
+    }
 }
---
+++ BeaverRuler/GRulerAppleWatch Extension/MeasuresListInterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  MeasuresListInterfaceController.swift
+//  GRulerAppleWatch Extension
+//
+//  Created by user on 10/29/17.
+//  Copyright © 2017 Sasha. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class MeasuresListInterfaceController: WKInterfaceController {
+
+}
